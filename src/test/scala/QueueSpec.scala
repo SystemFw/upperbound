91d65446f100f8004f41f001733243590a9b84ad
Fabio Labella <fabio.labella2@gmail.com>
2019-01-12 16:42:15 +0000

    Better tests

--- src/test/scala/QueueSpec.scala
+++ src/test/scala/QueueSpec.scala
@@ -11,29 +11,10 @@ import queues.Queue
 class QueueSpec extends BaseSpec {
 
   "A Queue should" - {
-
-    import DefaultEnv._
-    // Using property based testing for this assertion greatly
-    // increases the chance of testing all the concurrent
-    // interleavings.
-    "block on an empty queue until an element is available" in forAll {
-      (fst: Int, snd: Int) =>
-        def concurrentProducerConsumer =
-          for {
-            queue <- Stream.eval(Queue[IO, Int]())
-
-            consumer = Stream.eval(queue.dequeue)
-            producer = Stream.eval(fst.pure[IO] <* queue.enqueue(snd))
-            result <- consumer.merge(producer)
-          } yield result
-
-        val res = concurrentProducerConsumer.compile.toVector.unsafeRunSync
-
-        assert(res.contains(fst) && res.contains(snd))
-    }
-
     "dequeue the highest priority elements first" in forAll {
       (elems: Vector[Int]) =>
+        import DefaultEnv._
+
         def prog =
           Queue[IO, Int]()
             .map { q =>
@@ -50,6 +31,8 @@ class QueueSpec extends BaseSpec {
 
     "dequeue elements with the same priority in FIFO order" in forAll {
       (elems: Vector[Int]) =>
+        import DefaultEnv._
+
         def prog =
           Queue[IO, Int]()
             .map { q =>
@@ -65,29 +48,51 @@ class QueueSpec extends BaseSpec {
     }
 
     "fail an enqueue attempt if the queue is full" in {
+      import DefaultEnv._
+
       def prog =
         for {
           q <- Queue[IO, Int](1)
-          _ <- q.enqueue(1, 0)
-          _ <- q.enqueue(1, 0)
+          _ <- q.enqueue(1)
+          _ <- q.enqueue(1)
         } yield ()
 
       assertThrows[LimitReachedException](prog.unsafeRunSync)
     }
 
     "successfully enqueue after dequeueing from a full queue" in {
+      import DefaultEnv._
+
       def prog =
         for {
           q <- Queue[IO, Int](1)
-          _ <- q.enqueue(1, 0)
-          _ <- q.enqueue(2, 0).attempt
+          _ <- q.enqueue(1)
+          _ <- q.enqueue(2).attempt
           _ <- q.dequeue
-          _ <- q.enqueue(3, 0)
+          _ <- q.enqueue(3)
           r <- q.dequeue
         } yield r
 
       assert(prog.unsafeRunSync === 3)
     }
+  }
+
+  "block on an empty queue until an element is available" in {
+    val E = new Env
+    import E._
+
+    def prog =
+      Queue[IO, Unit]()
+        .flatMap { q =>
+          def prod = IO.sleep(1.second) >> q.enqueue(())
+          def consumer = q.dequeue.timeout(3.seconds)
+
+          prod.start >> consumer.as(true)
+        }
+
+    val res = prog.unsafeToFuture
+    env.tick(3.seconds)
+    res.map(r => assert(r))
   }
 
   "If a dequeue gets canceled before an enqueue, no elements are lost in the next dequeue" in {
@@ -103,7 +108,7 @@ class QueueSpec extends BaseSpec {
       } yield true
 
     val res = prog.unsafeToFuture
-    env.tick(4.seconds)
+    env.tick(3.seconds)
     res.map(r => assert(r))
   }
 }
