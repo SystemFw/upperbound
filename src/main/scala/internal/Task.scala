f5588a37bb2435bf32892717bec4c4d2cad7a622
Fabio Labella <fabio.labella2@gmail.com>
2021-09-07 01:22:27 +0100

    Address corner case when completing Task.result

--- src/main/scala/internal/Task.scala
+++ src/main/scala/internal/Task.scala
@@ -6,7 +6,6 @@ import fs2._
 import cats.syntax.all._
 import cats.effect.syntax.all._
 
-
 // This will be used if I follow the implementation strategy
 // of enqueing tasks directly, and having Limiter's background stream
 // execute them.
@@ -22,6 +21,11 @@ private[upperbound] case class Task[F[_]: Concurrent, A](
     result: Deferred[F, Outcome[F, Throwable, A]],
     stopSignal: Deferred[F, Unit]
 ) {
+  // ok other issue here: if this gets canceled before `task` runs, `result`
+  // will not be set to canceled (guaranteeCase doesn't run), so if we then wait
+  // on result in waitResult's onCancel logic, we will deadlock, we can probably
+  // set result to canceled again after stopSignal.get with >>
+  // the other idea is to use racePair or raceOutcome as the sole combinator
   /**
     * Packages `task` for later execution.
     * Cannot fail.
@@ -32,7 +36,13 @@ private[upperbound] case class Task[F[_]: Concurrent, A](
     task
       .guaranteeCase(result.complete(_).void)
       .redeem(_ => (), _ => ())
-      .race(stopSignal.get)
+      .race {
+        stopSignal.get >>
+        // this handles the corner case where `task` was canceled before its
+        // finaliser was installed, to make sure `result` is always complete.
+        // If `task` has already populated `result`, this becomes a no-op
+        result.complete(Outcome.canceled).void
+      }
       .void
 
   def cancelationRequested: F[Boolean] =
@@ -40,7 +50,9 @@ private[upperbound] case class Task[F[_]: Concurrent, A](
 
   // Ok, interesting question, should waitResult backpressure on canceling `task`?
   // If `task` is running, yes, but if task is still queued, we probably don't want
-  // to wait until it gets dequeued. How to detect that is an open question, since
+  // to wait until it gets dequeued. This affects whether after triggering stopSignal we want
+  // to wait on result again (for a Canceled outcome) or not
+  // How to detect that is an open question, since
   // the whole thing is based on the premise that we can't delete a
   // given task from the priority queue at will.
   // Maybe `executable` should set a flag, which will indicate it's been dequeued and about to execute:
@@ -50,7 +62,7 @@ private[upperbound] case class Task[F[_]: Concurrent, A](
     * Completes when `task` does.
     * Canceling `waitResult` cancels `task`, (respecting
     * cancelation backpressure? see question above)
-    * Cancels itself if `task` gets canceled externally.
+    * Cancels itself if `task` gets canceled externally. (is this absolutely necessary? generally not supported)
     */
   def waitResult: F[A] =
     result.get
